8dcdeb17a1e72de9bef29ca6f2d2d201c2ae5895
benjamin.buleon <benjamin.buleon@gojob.com>
2023-06-28 15:43:20 +0200

    add barbar and signature_help

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # My Neovim config
 
 This repository contains the neovim configuration that I use
+
--- lua/base.lua
+++ lua/base.lua
@@ -1,13 +1,13 @@
-vim.cmd('autocmd!')
+vim.cmd "autocmd!"
 
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 
 vim.wo.number = true
 vim.wo.relativenumber = true
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 vim.opt.title = true
 vim.opt.autoindent = true
@@ -19,9 +19,9 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
-vim.opt.shell = 'zsh'
-vim.opt.backupskip = '/tmp/*./private/tmp/*'
-vim.opt.inccommand = 'split'
+vim.opt.shell = "zsh"
+vim.opt.backupskip = "/tmp/*./private/tmp/*"
+vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 vim.opt.smarttab = true
 vim.opt.breakindent = true
@@ -30,13 +30,13 @@ vim.opt.tabstop = 2
 vim.opt.ai = true -- auto indent
 vim.opt.si = true -- smart indent
 vim.opt.wrap = false -- no wrap lines
-vim.opt.backspace = 'start,eol,indent'
-vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
-vim.opt.wildignore:append { '*/node_modules/*' }
+vim.opt.backspace = "start,eol,indent"
+vim.opt.path:append { "**" } -- Finding files - Search down into subfolders
+vim.opt.wildignore:append { "*/node_modules/*" }
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
+  pattern = "*",
+  command = "set nopaste",
 })
 
-vim.opt.formatoptions:append { 'r' }
+vim.opt.formatoptions:append { "r" }
